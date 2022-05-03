936fcb3e714a440c0718e859e303f4ee4d803638
Your Name <you@example.com>
2022-05-03 13:49:13 +0530

    Updates

---
+++ precache-manifest.1817c972e65555abdd941088d005a05e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccc13d45dd47e02c7a8975e335461aa",
+    "url": "/acadment/index.html"
+  },
+  {
+    "revision": "d0f4430c09b8b96af46d",
+    "url": "/acadment/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "3d5880e76ebe21012cc3",
+    "url": "/acadment/static/js/2.d31575a1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/acadment/static/js/2.d31575a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f4430c09b8b96af46d",
+    "url": "/acadment/static/js/main.2532b54a.chunk.js"
+  },
+  {
+    "revision": "5ae398b0477a9642686c",
+    "url": "/acadment/static/js/runtime-main.b8e07816.js"
+  }
+]);
